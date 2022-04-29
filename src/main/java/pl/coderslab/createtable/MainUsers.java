4bef2061b8422599ad283cd9ac36ef429ee8a53b
Ritagnieszka <adanecka@yahoo.pl>
2022-04-29 12:06:19 +0200

    creating table

--- src/main/java/pl/coderslab/createtable/MainUsers.java
+++ src/main/java/pl/coderslab/createtable/MainUsers.java
@@ -1,3 +1,26 @@
 package pl.coderslab.createtable;
 
+import pl.coderslab.dbutil.DBUtil;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
 public class MainUsers {
+    private static final String CREATE_TABLE_USER = "CREATE TABLE users (\n" +
+            "    id INT(11) NOT NULL AUTO_INCREMENT,\n" +
+            "    email VARCHAR(255) UNIQUE,\n" +
+            "    username VARCHAR(255),\n" +
+            "    password VARCHAR(60),\n" +
+            "    PRIMARY KEY (id)\n" +
+            ");";
+
+    public static void main(String[] args) {
+        try (Connection connection = DBUtil.connect()) {
+            PreparedStatement preStatement = connection.prepareStatement(CREATE_TABLE_USER);
+            preStatement.executeUpdate();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+}
--- src/main/java/pl/coderslab/dbutil/DBUtil.java
+++ src/main/java/pl/coderslab/dbutil/DBUtil.java
@@ -1,2 +1,15 @@
-package pl.coderslab.dbutil;public class DBUtil {
+package pl.coderslab.dbutil;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DBUtil {
+    private static final String DB_URL = "jdbc:mysql://localhost:3306/workshop2?useSSL=false&characterEncoding=utf8&serverTimezone=UTC";
+    private static final String DB_USER = "root";
+    private static final String DB_PASS = "coderslab";
+
+    public static Connection connect() throws SQLException {
+        return DriverManager.getConnection(DB_URL, DB_USER, DB_PASS);
+    }
 }
