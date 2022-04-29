e5122466b15fe0e659afff6aa28f427ec532d4cc
Ritagnieszka <adanecka@yahoo.pl>
2022-04-29 18:07:15 +0200

    add the create method

--- src/main/java/pl/coderslab/dao/MainDao.java
+++ src/main/java/pl/coderslab/dao/MainDao.java
@@ -1,5 +1,30 @@
 package pl.coderslab.dao;
 
-public class MainDao {
+import pl.coderslab.entity.User;
 
+public class MainDao {
+    public static void main(String[] args) {
+
+        // test metody creat
+        UserDao userDao = new UserDao();
+
+        User user1 = new User();
+        user1.setUserName("Anna");
+        user1.setEmail("anka@onet.pl");
+        user1.setPassword("ankaK123");
+
+        User user2 = new User();
+        user2.setUserName("Karol");
+        user2.setEmail("koral@wp.pl");
+        user2.setPassword("7842ako");
+
+        User user3 = new User();
+        user3.setUserName("Marcin");
+        user3.setEmail("marcin@gmail.com");
+        user3.setPassword("goral456J");
+
+        userDao.create(user1);
+        userDao.create(user2);
+        userDao.create(user3);
+    }
 }
--- src/main/java/pl/coderslab/dao/UserDao.java
+++ src/main/java/pl/coderslab/dao/UserDao.java
@@ -1,11 +1,48 @@
 package pl.coderslab.dao;
 
+import org.mindrot.jbcrypt.BCrypt;
+import pl.coderslab.dbutil.DBUtil;
+import pl.coderslab.entity.User;
+
+import java.sql.*;
+
+
 public class UserDao {
     private static final String CREATE_USER_QUERY = "INSERT INTO users(username, email, password) VALUES (?, ?, ?);";
     private static final String READ_USER_QUERY = "SELECT * FROM users WHERE id = ?;";
     private static final String UPDATE_USER_QUERY = "UPDATE users SET username = ?, email = ?, password = ? WHERE id = ?;";
     private static final String FIND_ALL_USERS_QUERY = "SELECT * FROM users;";
     private static final String DELETE_USER_QUERY = "DELETE FROM users WHERE id = ?;";
+
+
+    // metoda creat - korzysta z metody BCrypt.hashpw
+    public User create(User user) {
+        try (Connection conn = DBUtil.connect()) {
+            PreparedStatement preStatement = conn.prepareStatement(CREATE_USER_QUERY, PreparedStatement.RETURN_GENERATED_KEYS);
+            preStatement.setString(1, user.getUserName());
+            preStatement.setString(2, user.getEmail());
+            preStatement.setString(3, hashPassword(user.getPassword()));
+            preStatement.executeUpdate();
+            ResultSet rs = preStatement.getGeneratedKeys();
+            if (rs.next()) {
+                user.setId(rs.getInt(1));
+                System.out.println("Inserted ID: " + rs.getInt(1));
+            }
+            return user;
+        } catch (SQLException e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+
+    // metoda BCrypt.hashpw
+    public String hashPassword(String password) {
+        return BCrypt.hashpw(password, BCrypt.gensalt());
+    }
+
+
+
+
 
 
 }
